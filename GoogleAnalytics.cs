d440f5eb4990dc49b8b16032fb99b0b96f87f15e
Jared Messenger <jamessenger@gmail.com>
2012-11-05 22:36:10 -0800

    started adding in the request parameters and cookie data

--- GoogleAnalytics.cs
+++ GoogleAnalytics.cs
@@ -1,12 +1,33 @@
 using UnityEngine;
 using System.Collections;
 
+/*
+ * 		requestParams["utmac"]  = // Account String UA-XXXXX
+ * 		requestParams["utmhn"]  = // Hostname
+		requestParams["utmdt"]  = // Page title - Possible use this as a level/map/section
+		requestParams["utme"]   = // Event Parameters
+		requestParams["utmt"]	= // Type of request: page, event, transaction, item, custom variable, default is page
+		requestParams["utmp"]	= // Page request of the current page
+		requestParams["utmcr"]  = // Language encoded for the browser
+		requestParams["utmfl"]  = // flash version
+		requestParams["utmn"]	= // Unique ID generated for each GIF request to prevent caching
+		requestParams["utmsc"]	= // Screen color depth
+		requestParams["utmsr"]	= // Screen resolution
+		requestParams["utmwv"]  = // Tracking code version
+		requestParams["utmul"]	= // Browser language http://www.metamodpro.com/browser-language-codes
+		requestParams["utmcc"]  = // Cookie
+ * 
+ */
+
 public class GoogleAnalytics : MonoBehaviour {
 	
-	public string PropertyID;
-	public string DefaultURL;
+	public string propertyID;
+	public string defaultURL;
 	
 	public static GoogleAnalytics instance;
+	
+	private Hashtable requestParam = new Hashtable();
+	private long sessionStartTime;
 	
 	void Awake()
 	{
@@ -21,7 +42,10 @@ public class GoogleAnalytics : MonoBehaviour {
 	
 	public void Start(){
 		//Get the player prefs last time played and current time
-
+		sessionStartTime = GetEpochTime();
+		
+		requestParam["utmac"] = propertyID;
+		requestParam["utmhn"] = defaultURL;
     }
 	
 	public void SetCustomVar(int index, string name, string value, int scope)
@@ -33,34 +57,81 @@ public class GoogleAnalytics : MonoBehaviour {
 	
 	public void TrackLevel()
 	{
-		Debug.Log(Application.loadedLevelName);	
+		string levelName = Application.loadedLevelName;
+		requestParams["utmt"] = GoogleTrackType.GALevel;
+		requestParams["utmn"] = Random.Range(1000000000,2000000000).ToString();
+		
+		Dispatch();
+		
+		// Remove so the slate it clean for new tracking
+		requestParam.Remove("utmt");
+		requestParam.Remove("utmn");
 	}
 	
 	public void TrackEvent(string category, string label, string action, int value)
 	{
-		Debug.Log(category);	
+		requestParams["utmt"] = GoogleTrackType.GAEvent;
+		requestParams["utmn"] = Random.Range(1000000000,2000000000).ToString();
+		
+		Dispatch();
+		
+		// Remove so the slate it clean for new tracking
+		requestParam.Remove("utmt");
+		requestParam.Remove("utmn");
 	}
 	
 	public void TrackTiming()
 	{
 	 	// https://developers.google.com/analytics/devguides/collection/gajs/gaTrackingTiming
-		Debug.Log("Timer");
+		requestParams["utmt"] = GoogleTrackType.GATiming;
+		requestParams["utmn"] = Random.Range(1000000000,2000000000).ToString();
+		
+		Dispatch();
+		
+		// Remove so the slate it clean for new tracking
+		requestParam.Remove("utmt");
+		requestParam.Remove("utmn");
 	}
 	
 	public void Dispatch()
 	{
 		// Send the data to the Google Servers
-		Debug.Log("Sending data to Google");
+    	string urlParams = BuildRequestString();
+		string url = "http://www.google-analytics.com/__utm.gif?" + urlParams;
+		new WWW(url);
 	}
 	
-	private long PlayerID()
+	private long DeviceIdentifier()
 	{
         return Hash (SystemInfo.deviceUniqueIdentifier );
 	}
 	
 	private void SetCookieData()
 	{
+		long currentTime  = GetEpochTime();
+		long domainHas = Hash(defaultURL);
 		
+		// __utma Identifies unique Visitors
+		string _utma   = domainHash + "." + DeviceIdentifier + "." + TimeFirstPlayed + "." + 
+			TimeLastPlayed + "." + sessionStartTime + "." + NumPlays + WWW.EscapeURL(";") + WWW.EscapeURL("+");
+
+		// __utmz Referral information in the cookie
+		string cookieUTMZstr = "utmcsr" + WWW.EscapeURL("=") + "(direct)" + WWW.EscapeURL("|") + 
+			"utmccn" + WWW.EscapeURL("=") + "(direct)" + WWW.EscapeURL("|") + 
+			"utmcmd" + WWW.EscapeURL("=") + "(none)" + WWW.EscapeURL(";");
+		
+		string _utmz = domainHash + "." + currentTime + "." + NumPlays + ".1." + cookieUTMZstr;
+		
+		requestParams["utmcc"] = "__utma" + WWW.EscapeURL("=") + _utma + "__utmz" + WWW.EscapeURL("=") + _utmz;
+	}
+	
+	private string BuildRequestString()
+	{
+		List<string> args = new List<string>();
+		foreach( string key in requestParams.Keys ) {
+			args.Add( key + "=" + requestParams[key] );	
+		}
+		return string.Join("&", args.ToArray());	
 	}
 	
 	private long Hash(string url)
@@ -88,4 +159,10 @@ public class GoogleAnalytics : MonoBehaviour {
 		
 		return epochTime;
 	}
+}
+
+public enum GoogleTrackType{
+	GALevel,
+	GAEvent,
+	GATiming,
 }
